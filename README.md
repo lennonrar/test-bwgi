5fb8d47294840f7cf010daf08e02328425ac65e2
Lennon <lennon.ribeiro@protonmail.com>
2024-08-13 23:39:28 -0300

    chore: update README.md; style fixes on reconcile account

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# test-bwgi
+## Code Challenge from bwgi
+To run the methods as modules:
+
+```python -m <module_name>```
+
+or import one by one. Example:
+
+```from reconcile_accounts.module import reconcile_accounts```
--- reconcile_accounts/main.py
+++ reconcile_accounts/main.py
@@ -4,13 +4,14 @@ from pprint import pprint
 
 from module import reconcile_accounts
 
+
 def main() -> None:
-    transactions1 = list(csv.reader(Path('transactions1.csv').open()))
-    transactions2 = list(csv.reader(Path('transactions2.csv').open()))
+    transactions1 = list(csv.reader(Path("transactions1.csv").open()))
+    transactions2 = list(csv.reader(Path("transactions2.csv").open()))
     out1, out2 = reconcile_accounts(transactions1, transactions2)
     pprint(out1)
     pprint(out2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
